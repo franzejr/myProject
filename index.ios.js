dfbe37bcc659f1932fa6b2cc7eafcaa194ae9201
Franzé Jr <franzejr@gmail.com>
2016-12-30 11:09:33 -0300

    Change the text

--- index.ios.js
+++ index.ios.js
@@ -18,6 +18,7 @@ export default class myProject extends Component {
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
+          Changed text here!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
